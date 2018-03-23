a7092d928eaec57882da855f07002a58abdf3d4e
Shalin Scupham <shalin.scupham@gmail.com>
2018-03-23 17:23:56 -0400

    Add cookie setter/getter methods, serve guid & color cookies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./blue-red-ball.jpg";
 import BallImage from "./ball_image";
+import { createCookie, getCookie, guid } from "./cookie";
 import "./App.css";
 
 class App extends Component {
@@ -10,8 +11,12 @@ class App extends Component {
   }
 
   render() {
-    const color = this.randomColor();
-
+    getCookie("guid")
+      ? console.log(getCookie("guid"))
+      : console.log("new user!");
+    const ballColor = this.randomColor();
+    createCookie("color", ballColor, 30);
+    createCookie("guid", guid(), 30);
     return (
       <div className="App">
         <header className="App-header">
@@ -19,7 +24,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to Brooklyn Ball Co!</h1>
         </header>
 
-        <BallImage ballColor={color} />
+        <BallImage ballColor={ballColor} />
 
         <p className="App-intro" />
       </div>
---
+++ src/cookie.js
@@ -0,0 +1,34 @@
+export const createCookie = (name, value, days) => {
+  let expires;
+  if (days) {
+    let date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toGMTString();
+  } else {
+    expires = "";
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+};
+
+export const getCookie = name => {
+  if (document.cookie.length > 0) {
+    let c_start = document.cookie.indexOf(name + "=");
+    if (c_start !== -1) {
+      c_start = c_start + name.length + 1;
+      let c_end = document.cookie.indexOf(";", c_start);
+      if (c_end === -1) {
+        c_end = document.cookie.length;
+      }
+      return unescape(document.cookie.substring(c_start, c_end));
+    }
+  }
+  return "";
+};
+
+export const guid = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
